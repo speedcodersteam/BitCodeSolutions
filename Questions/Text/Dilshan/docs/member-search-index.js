d41790a214dd8d4f5bfb505ccd7aa9394a43c6f9
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-01-31 21:15:28 +0530

    added documentation

---
+++ Questions/Text/Dilshan/docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"me.karunarathne.BitCode","c":"Text","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"me.karunarathne.BitCode","c":"Text","l":"Text()","u":"%3Cinit%3E()"}];updateSearchResults();
---
+++ Questions/Text/Dilshan/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"me.karunarathne.BitCode","l":"Text"}];updateSearchResults();
