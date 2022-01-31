7ca3a6e5703b90eb22e40e7e2f4ee6c4628d9546
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-01-31 12:32:12 +0530

    Text.java in progress

--- Questions/Text/Dilshan/src/me/karunarathne/Text.java
+++ Questions/Text/Dilshan/src/me/karunarathne/Text.java
@@ -26,6 +26,43 @@
 
 package me.karunarathne;
 
+import java.util.Scanner;
+
 public class Text {
-    
+    public static void main(String[] args) {
+        
+    }
+
+    private static void takeInput (boolean enablePrompt) {
+        try (Scanner scanner = new Scanner (System.in)) { 
+            if (enablePrompt) System.out.print ("Enter the char range > ") ;
+            
+            String rangeStr[] = scanner.nextLine().split(" ") ;
+            int start = Integer.parseInt(rangeStr[0]) ;
+            int end = Integer.parseInt(rangeStr[1]) ;
+
+            if (! checkInputRange (start, end)) {
+                System.exit(0);
+            }
+
+            if (enablePrompt) System.out.print ("Enter the paragraph > ") ;
+            String paragraph = scanner.nextLine() ;
+        }
+    }
+
+    private static boolean checkInputRange (int start, int end) {
+        if (start < 0) {
+            System.out.println("Error: Start Number");
+            return false ;
+        }
+        if (end < 5) {
+            System.out.println("Error: End Number");
+            return false ;
+        }
+        if (start > end) {
+            System.out.println("Error: Range");
+            return false ;
+        }
+        return true ;
+    }
 }
