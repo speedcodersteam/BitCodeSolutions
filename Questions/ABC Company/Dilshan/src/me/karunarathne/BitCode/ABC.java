14583581203284b7b92aa39db67655750cd8ca01
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-02-14 17:12:15 +0530

    added sol

---
+++ Questions/ABC Company/Dilshan/src/me/karunarathne/BitCode/ABC.java
@@ -0,0 +1,26 @@
+package me.karunarathne.BitCode ;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ABC {
+    private static ArrayList <String> inputs ;
+    public static void main(String[] args) {
+        
+    }
+
+    private static void humanResources () {
+        
+    }
+
+    private static void takeInput () {
+        try (Scanner scanner = new Scanner (System.in)) {
+                while (true) {
+                String input = scanner.nextLine() ;
+                if (input.equalsIgnoreCase("e")) {
+                    break ;
+                }
+            }
+        }
+    }
+}
--- Questions/ABC Company/README.md
+++ Questions/ABC Company/README.md
@@ -21,3 +21,14 @@ The single line should be represented the productivity of the employee. If the o
 ### Sample Output
 20  
 30
+
+## Important
+Feel free to use any programming language. (Eg: **Java** or **C++**)  
+If the user's input is not correct, you should throw an exception. For those who are unfamiliar with exception handling, just print an Error message.  
+
+#### Question was taken from BitCode V 3.0
+Organized by  
+<b>BITSA</b>  
+Department of Information Systems  
+Faculty of Management  
+Rajarata University of Sri Lanka  
